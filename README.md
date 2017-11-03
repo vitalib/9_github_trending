610991f654577f4e706a303f734fb1159d289a3a
Vitali Baranov <vitali.baranov@gmail.com>
2017-11-03 12:56:22 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
-# Github Trends
+# Github Trends 
+Displays 20 repositories with biggest quantity of stars created for the last 7 days
 
-[TODO. There will be project description]
+# Quick Start
+Python3 is required.
+
+Example on Linux
+```bash
+$python3 github_trending.py
+Top repositories for the last seven days: 
+1 https://github.com/ufoym/deepo
+	 Stars = 1575
+	 Open issues = 7
+2 https://github.com/Alimei/hangzhouYunQi2017ppt
+	 Stars = 1490
+	 Open issues = 5
+3 https://github.com/tkarras/progressive_growing_of_gans
+	 Stars = 1122
+	 Open issues = 2
+4 https://github.com/tatsuyah/vehicle-detection
+	 Stars = 608
+	 Open issues = 0
+5 https://github.com/ShaunSheep/Android_100_TOP-Projects
+	 Stars = 523
+	 Open issues = 4
+6 https://github.com/o1lab/xmysql
+	 Stars = 431
+	 Open issues = 0
+7 https://github.com/wangyu-/tinyFecVPN
+	 Stars = 372
+	 Open issues = 4
+8 https://github.com/kizitonwose/Time
+	 Stars = 329
+	 Open issues = 0
+...
+```
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,47 @@
+import requests
+import datetime
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_starting_date(days_delta):
+    starting_date = datetime.datetime.today()-datetime.timedelta(days_delta)
+    return starting_date.strftime("%Y-%m-%d")
+
+
+def get_trending_repositories(top_size, days_delta):
+    starting_date = get_starting_date(days_delta)
+    url = 'https://api.github.com/search/repositories'
+    parameters = {
+                  'q': 'created:>{}'.format(starting_date),
+                  'sort': 'stars',
+                  'order': 'desc',
+                  'per_page': '{}'.format(top_size),
+                  }
+    request = requests.get(url, parameters)
+    return request.json()['items']
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    url_pattern = "https://api.github.com/repos/{}/{}/issues"
+    url = url_pattern.format(repo_owner, repo_name)
+    open_issues = requests.get(url, {'state': 'open'}).json()
+    return len(open_issues)
+
+
+def print_repos_and_issues(repositories):
+    print('Top repositories for the last seven days: ')
+    for place, repo in enumerate(repositories, start=1):
+        print(place, repo['html_url'])
+        print('\t Stars = {}'.format(repo['watchers']))
+        issues_amount = get_open_issues_amount(
+            repo['owner']['login'],
+            repo['name'],
+        )
+        print('\t Open issues = {}'.format(issues_amount))
+
 
 if __name__ == '__main__':
-    pass
+    trending_repositories = get_trending_repositories(
+                                                top_size=20,
+                                                days_delta=7,
+                                                      )
+    print_repos_and_issues(trending_repositories)
